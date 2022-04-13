9aee14884089cb14636dc22c5db0cb33697b83fd
galen94 <90521087+galen94@users.noreply.github.com>
2022-04-13 17:19:14 -0400

    Revert "Added reducer, action and service for alerts. Still adjusting.  Need to get node side working to test."
    This reverts commit 5070e3f0734ae2b396f4ce9e8063ee46aedea94a.

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import NavigationReducer from "../reducers/navigation-reducer";
-import AlertsReducer from "../reducers/alerts-reducer";
 import {combineReducers, createStore} from "redux";
 import {Provider} from "react-redux";
 import NavigationBar from "./navigation-bar";
 import AlertsBar from "./alerts-bar";
 import {Outlet} from "react-router-dom";
 
-const reducers = combineReducers({navigationData: NavigationReducer, alerts: AlertsReducer});
+const reducers = combineReducers({navigationData: NavigationReducer});
 const store = createStore(reducers);
 
 const Home = () => {
